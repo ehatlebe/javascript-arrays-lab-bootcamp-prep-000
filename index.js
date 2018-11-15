406bdfe04b73d8705be3f1088fa871ee3f66d032
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-15 17:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(kittensAll) {
-  let kittensNew = ["Babs"];
-  kittens = kittens.concat(kittensNew);
+function destructivelyAppendKitten(kittensNew) {
+  kittensNew = ["Ralph"];
+  kittens = kittens.;
   return kittens;
 };
