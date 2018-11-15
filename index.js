033135f245258c9cddaabdb9ae3c4860f628ee98
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-15 19:20:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,53 +6,58 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 };
-console.log(kittens);
 console.log(`destructivelyAppendKitten: ${destructivelyAppendKitten()}`);
+console.log(`kittens: ${kittens}`);
 
 function destructivelyPrependKitten(name) {
   name = "Bob";
   kittens.unshift(name);
   return kittens;
 };
-console.log(kittens);
 console.log(`destructivelyPrependKitten: ${destructivelyPrependKitten()}`);
+console.log(`kittens: ${kittens}`);
 
 function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
 };
-console.log(kittens);
 console.log(`destructivelyRemoveLastKitten: ${destructivelyRemoveLastKitten()}`);
+console.log(`kittens: ${kittens}`);
 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 };
-console.log(kittens);
 console.log(`destructivelyRemoveFirstKitten: ${destructivelyRemoveFirstKitten()}`);
+console.log(`kittens: ${kittens}`);
 
 function appendKitten(name) {
   name = "Broom";
   let kittensAppended = [...kittens, name];
   return kittensAppended;
 };
-console.log(kittens);
 console.log(`appendKitten: ${appendKitten()}`);
+console.log(`kittens: ${kittens}`);
+console.log(`kittensAppended: ${kittensAppended}`);
 
 function prependKitten(name) {
   name = "Arnold";
   let kittensPrepended = [name, ...kittens];
   return kittensPrepended;
 };
-console.log(kittens);
 console.log(`prependKitten: ${prependKitten()}`);
+console.log(`kittens: ${kittens}`);
+console.log(`kittensPrepended: ${kittensPrepended}`);
 
 function removeLastKitten() {
   kittensRemoveLast = kittens.pop();
   return kittensRemoveLast;
 };
-console.log(kittens);
 console.log(`removeLastKitten: ${removeLastKitten()}`);
+console.log(`kittens: ${kittens}`);
+console.log(`kittensRemoveLast: ${kittensRemoveLast}`);
 
 //function removeFirstKitten() {};
 //console.log(`removeFirstKitten: ${removeFirstKitten()}`);
+//console.log(`kittens: ${kittens}`);
+//console.log(`kittens: ${kittens}`);
